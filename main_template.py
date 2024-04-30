ca082c3932ba4e11e76511ca8763f6eafc1222f7
Vineeth Krishnamurthy <vineeth@Vineeths-MacBook-Pro.local>
2024-04-30 09:54:51 -0400

    add new grabdata endpoint

--- main_template.py
+++ main_template.py
@@ -37,7 +37,7 @@ class GenerateBudget(Resource):
                                 type: string
                                 description: The text in uppercase
         """
-        text = request.args.get('text')
+        user_message = request.args.get('text')
         user1 = client_data[0]
         transactions_input = str(user1)
 
@@ -47,17 +47,45 @@ class GenerateBudget(Resource):
             messages=[
                 {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
                 {"role": "user", "content": transactions_input},
-                {"role": "user", "content": "Given these monthly transactions, create a budget for me for next month"}
+                {"role": "user", "content": user_message}
             ]
         )
 
         output = response.choices[0].message.content
 
-        print(output)
-
         return jsonify(output)
 
+class GrabData(Resource):
+
+    def get(self):
+        """
+        This method responds to the GET request for this endpoint and returns the data in uppercase.
+        ---
+        tags:
+        - Text Processing
+        parameters:
+            - name: text
+              in: query
+              type: string
+              required: true
+              description: The text to be converted to uppercase
+        responses:
+            200:
+                description: A successful GET request
+                content:
+                    application/json:
+                      schema:
+                        type: object
+                        properties:
+                            text:
+                                type: string
+                                description: The text in uppercase
+        """
+
+        return jsonify(client_data)
+
 api.add_resource(GenerateBudget, "/generatebudget")
+api.add_resource(GrabData, "/grabdata")
 
 if __name__ == "__main__":
     app.run(debug=True)
