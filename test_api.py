14298555224a90f8c8c8742039efddab6c442ca5
Vineeth Krishnamurthy <vineeth@Vineeths-MacBook-Pro.local>
2024-04-30 15:08:48 -0400

    new crompt

--- test_api.py
+++ test_api.py
@@ -2,7 +2,7 @@ import requests
 
 base_url = "https://cache-money-api.onrender.com/generatebudget"
 
-params = {'text': "I want to save $100 next month"}
+params = {'text': "I want to spend 20% less"}
 
 response = requests.get(base_url, params=params)
 
