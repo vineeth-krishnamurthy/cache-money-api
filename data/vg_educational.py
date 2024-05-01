c2378e11752b56c29beed9e7159ab5cf3074c502
Jack Foster <jackfoster246@gmail.com>
2024-05-01 14:29:35 -0400

    provide article context

---
+++ data/vg_educational.py
@@ -0,0 +1,79 @@
+articles = """U.S. Treasury securities
+U.S. Treasury securities are direct debt obligations backed by the full faith and credit of the U.S. government. Interest can be paid at maturity or semiannually depending on the type of security. Treasuries usually are issued in $1,000 denominations.
+Yield
+
+Treasuries usually offer lower yields than other fixed income securities because their minimal risk makes them among the safest investments available.
+Types
+
+Treasury bills are issued with maturities of 52 weeks or less. They are issued at a discount and redeemed at face value. The difference is calculated as the taxable interest income.
+
+Treasury notes are issued with maturities of 2 to 10 years. Interest is paid every 6 months.
+
+Treasury bonds are issued with a maturity of more than 10 years, most commonly for a period of 30 years. Interest is paid every 6 months.
+
+Treasury Separate Trading of Registered Interest and Principal of Securities (STRIPS) are created when broker-dealers or the Treasury separates (“strips”) the interest and principal of a Treasury note or bond into separate components, which are then traded as zero-coupon securities. Investors buy STRIPS at a price below the face value of the securities and then receive the full amount when the STRIPS mature.
+
+Treasury Inflation-Protected Securities (TIPS) are issued in terms of 5, 10, and 30 years. The principal amount rises or falls depending on the consumer price index. TIPS pay interest semiannually at a fixed rate applied to the inflation-adjusted principal. At maturity, the holder is paid the adjusted principal or original principal, whichever is greater. The yield quoted on TIPS is exclusive of inflation or deflation. During periods of deflation, previous positive adjustments to the inflation factor will erode. This means that investors purchasing previously issued TIPS may experience a loss of principal.
+
+Treasury Floating Rate Notes (FRNs) are issued with a maturity of 2 years. The interest rate on a Treasury FRN, which resets weekly, is the sum of 2 components: the index rate, which is tied to the rate of a recently auctioned 13-week Treasury bill, and the spread, which is set when the FRN is first auctioned and remains fixed for the life of the FRN. As Treasury bill rates rise, the FRN’s interest payments will increase. Similarly, as Treasury bill rates fall, the FRN’s interest payments will decrease. Interest is paid quarterly. Floating Rate Notes may have a negative spread, which was set at the auction. This means that the yield on this floating rate note will be lower than the yield of the current 13-week Treasury bill. Vanguard does not offer the purchase of floating rate notes.
+Taxability
+
+The interest income on Treasury securities is subject to federal taxes but is exempt from state and local taxes.
+
+Treasury notes and bonds, when bought at a discount, may subject investors to capital gains taxes when sold or redeemed. Investors should consult a tax professional for additional information.
+
+Treasury STRIPS and TIPS investors must pay taxes on interest accrued or inflation protection added in the most recent year, even though no cash payments have been received. Investors should consult a tax professional for more information.
+Liquidity
+
+Vanguard Brokerage doesn’t make a market in Treasury securities. If you wish to sell your Treasury securities prior to maturity, Vanguard Brokerage can provide access to a secondary over-the-counter market. In general, the secondary market for outstanding Treasuries provides liquidity, and the spread between bid and offer is usually narrower than for other fixed income securities. Nevertheless, liquidity will vary depending on a specific bond’s features, lot size, and other market conditions. Treasuries sold prior to maturity may be subject to substantial gain or loss.
+Fees
+
+Vanguard Brokerage Services® doesn’t charge a commission for any Treasury order.
+Risks
+
+Treasury prices can rise or fall depending on interest rates. Interest rate changes generally have a greater effect on long-term Treasury prices.
+
+All bonds carry risk that the issuer will default or be unable to make timely payments of interest and principal. However, Treasuries carry minimal risk since they’re backed by the full faith and credit of the U.S. government.
+
+Occasionally, Treasuries have call provisions that allow the issuer to buy back the bonds at a fixed price before the stated maturity date. Issuers typically call bonds during periods of declining interest rates.
+
+Treasuries sold before maturity may face a substantial gain or loss. The secondary market may also be limited.
+Treasury auctions
+
+The U.S. Treasury sells securities through a schedule of regular public auctions, which determine the yield of the securities. It makes periodic adjustments to the auction calendar as its borrowing needs change.
+
+The Treasury announces the amount to be auctioned several days before the upcoming issue and other details, including the maturity and settlement dates. They are auctioned by competitive and noncompetitive bids. Competitive bids generally are placed by dealers and other institutions. Vanguard Brokerage offers only noncompetitive bids limited to $10 million per security per household in one auction. Competitive and noncompetitive bidders receive the same rate or yield at auction.
+
+Visit TreasuryDirect for more information.
+
+Learn about treasury auctions
+Does Vanguard offer I-Bonds?
+
+No, Vanguard Fixed Income Trading does not offer I-bonds; I-bonds are savings bonds and cannot be purchased at Vanguard. No brokerage firm can offer savings bonds unless they also act in the capacity of a bank. In general, savings bonds can only be purchased at local banks or directly through the U.S. Treasury savings bond program (TreasuryDirect.gov).
+
+However, the Fixed Income Trading Desk does offer Treasury Inflation-Protected Securities (TIPS), which can be confused with I-Bonds.
+Vanguard ETFs® (exchange-traded funds) are popular for several reasons: they’re low cost, have a history of strong performance and tax efficiency, and offer the flexibility of intraday pricing. And they just got even better thanks to fractional share investing.
+
+Fractional investing allows you to trade a Vanguard ETF® for any dollar amount you choose, regardless of the ETF’s share price. And if you need to withdraw your money in the future, you can redeem a specific dollar amount or a set number of shares.* 
+
+Invest like fund minimums don’t exist. If one share of an ETF is $100 and you have $50 to invest, you can purchase 1/2 of a share. If you have just $1 to invest, you can purchase 1/100 of a share. 
+Fractional investing makes it possible to:
+Invest by dollar amount
+
+Think in dollars, not in shares, when you trade Vanguard ETFs. Buy a Vanguard ETF for as little as $1, regardless of the ETF’s share price. And you don’t have to leave a balance in your settlement fund unless you want to.
+
+ 
+Withdraw exactly what you need
+
+Sell a specific dollar amount from your portfolio and withdraw exactly what you need—nothing more, nothing less.
+
+ 
+Rebalance by dollar amount
+
+Rebalance your portfolio by dollar amount without having to convert shares to dollars (or vice versa). For example, you can sell $100 of your stock ETF and purchase $100 of a bond ETF.
+
+ 
+Easily build a diversified portfolio
+
+Handpick your own Vanguard ETF from our list of low-cost U.S. and international ETFs, or build a diversified portfolio by investing in just 4 total market ETFs.
+"""
