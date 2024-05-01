817b0b034848527146ffca3a7ffd28d9a324708c
Jack Foster <jackfoster246@gmail.com>
2024-05-01 13:45:44 -0400

    type text

--- main_template.py
+++ main_template.py
@@ -222,7 +222,7 @@ class GenerateChatBotResponse(Resource):
         openai_response = client.chat.completions.create(
             model="gpt-3.5-turbo-0125",
             messages=messages,
-            response_format="text"
+            response_format={ "type": "text" }
         )
         chat_history = {
             "role": "user",
