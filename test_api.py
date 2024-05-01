7864b2e2882bc03b33883399fafc1c6d1f36b6da
Vineeth Krishnamurthy <vineeth@Vineeths-MacBook-Pro.local>
2024-05-01 10:36:57 -0400

    change ai prompt

--- test_api.py
+++ test_api.py
@@ -5,7 +5,7 @@ base_url = "https://cache-money-api.onrender.com/generate_chat_response"
 # base_url = "http://127.0.0.1:5000/current_spending"
 
 # params = {'userID': "Doug"}
-params = {'userID': 'Doug', 'text': 'What kind of budget should I use'}
+params = {'userID': 'Doug', 'text': 'How should I invest my money'}
 
 response = requests.get(base_url, params=params)
 
